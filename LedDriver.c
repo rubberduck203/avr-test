95f7c0740ddc1af10758d0e445d8bc051a9c71b6
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2017-09-14 19:59:04 -0400

    Extracted method to calculate bitmask

--- LedDriver.c
+++ LedDriver.c
@@ -5,14 +5,19 @@ void LedDriver_Create(volatile uint8_t* ledsAddress)
     *ledsAddress = 0;
 }
 
-void TurnLedOn(volatile uint8_t* ledsAddress, uint8_t ledNumber)
+uint8_t LedNumberToBitMask(uint8_t ledNumber)
 {
     /* 
      * Led numbering is 1-based indexed
      * but bitmasking by index is 0-based.
-     * i.e. 2^0 == 1 == 0b0000_0001
-     * and 2^4 == 16 == 0b0001_0000
+     * i.e. 2^0 ==  1 == 0b0000_0001
+     * and  2^4 == 16 == 0b0001_0000
      * So we must translate to a zero index before left shifting.
      */
-    *ledsAddress |= (1 << (ledNumber - 1));
+    return 1 << (ledNumber - 1);
+}
+
+void TurnLedOn(volatile uint8_t* ledsAddress, uint8_t ledNumber)
+{
+    *ledsAddress |= LedNumberToBitMask(ledNumber);
 }
