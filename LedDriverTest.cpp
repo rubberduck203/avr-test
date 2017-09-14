199a7a27dccd01d53381157f83bf920094b2c2cd
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2017-09-14 19:59:12 -0400

    Extracted test setup

--- LedDriverTest.cpp
+++ LedDriverTest.cpp
@@ -5,55 +5,49 @@ extern "C"
     #include "LedDriver.h"
 }
 
-TEST_GROUP(LedDriver)
+TEST_GROUP(LedDriverCreation)
 {
 };
 
-TEST(LedDriver, LedsAreOffAfterCreate)
+TEST(LedDriverCreation, LedsAreOffAfterCreate)
 {
   uint8_t virtualLeds = 0xff;
   LedDriver_Create(&virtualLeds);
   LONGS_EQUAL(0, virtualLeds);
 }
 
+uint8_t virtualLeds;
+TEST_GROUP(LedDriver)
+{
+  void setup()
+  {
+    virtualLeds = 0xff;
+    LedDriver_Create(&virtualLeds);
+  }
+};
+
 TEST(LedDriver, TurnOnLED1)
 {
-  uint8_t virtualLeds = 0xff;
-  LedDriver_Create(&virtualLeds);
-  
   TurnLedOn(&virtualLeds, 1);
-
   BYTES_EQUAL(0x01, virtualLeds);
 }
 
 TEST(LedDriver, TurnOnLed2)
 {
-  uint8_t virtualLeds = 0xff;
-  LedDriver_Create(&virtualLeds);
-
   TurnLedOn(&virtualLeds, 2);
-
   BYTES_EQUAL(0x02, virtualLeds);
 }
 
 TEST(LedDriver, ALightAlreadyOnStaysOn)
 {
-  uint8_t virtualLeds = 0xff;
-  LedDriver_Create(&virtualLeds);
-
   TurnLedOn(&virtualLeds, 1);
   TurnLedOn(&virtualLeds, 2);
-
   BYTES_EQUAL(0x03, virtualLeds);
 }
 
 TEST(LedDriver, TurnOnSecondByteLed)
 {
-  uint8_t virtualLeds = 0xff;
-  LedDriver_Create(&virtualLeds);
-
   TurnLedOn(&virtualLeds, 8);
-
   BYTES_EQUAL(0x80, virtualLeds);
 }
 
