0b5557a1ed31211315641c9427e6c26b6f072d52
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2017-10-04 09:30:29 -0400

    Extracted indexToBitmask method

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -11,17 +11,21 @@ void LedsOff(leds_t* leds)
     *leds = OFF;
 }
 
-void LedOn(leds_t* leds, uint8_t ledNumber)
+leds_t indexToBitmask(uint8_t ledNumber)
 {
     /* 
      * Leds are 1 indexed, so we correct the index
      * then shift a one into that bit of an integer
-     * finally, we apply the bitmask against the existing state
      */
-    *leds |= (1 << (ledNumber - 1));
+    return 1 << (ledNumber - 1);
+}
+
+void LedOn(leds_t* leds, uint8_t ledNumber)
+{
+    *leds |= indexToBitmask(ledNumber);
 }
 
 void LedOff(leds_t* leds, uint8_t ledNumber)
 {
-    *leds &= ~(1 << (ledNumber - 1));
+    *leds &= ~indexToBitmask(ledNumber);
 }
