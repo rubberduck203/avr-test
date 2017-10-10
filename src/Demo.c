2a2ecb04dd4aa5e597ce891454458f4866b57913
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2017-10-10 15:52:54 -0400

    Mock out avr & util dependencies

--- src/Demo.c
+++ src/Demo.c
@@ -1,22 +1,6 @@
-#include "LedDriver.h"
-#include <avr/io.h>
-#include <util/delay.h>
+#include "Run.h"
 
 int main(void) 
 {
-    //Just here to make sure we can link.
-    // Implementation will come later.
-    
-    PORTE.DIR = 0xff; //config entire port for output
-
-    while (1)
-    {
-        volatile leds_t* leds = &PORTE.OUT;
-        LedsOff(leds);
-        _delay_ms(1000);
-        LedsOn(leds);
-        _delay_ms(1000);
-    }
-    
-    return 0;
+    return Run();
 }
---
+++ src/Run.c
@@ -0,0 +1,22 @@
+#include "LedDriver.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
+int Run(void) 
+{
+    //Just here to make sure we can link.
+    // Implementation will come later.
+    
+    PORTE.DIR = 0xff; //config entire port for output
+
+    while (1)
+    {
+        volatile leds_t* leds = &PORTE.OUT;
+        LedsOff(leds);
+        _delay_ms(1000);
+        LedsOn(leds);
+        _delay_ms(1000);
+    }
+    
+    return 0;
+}
